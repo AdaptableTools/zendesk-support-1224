549a7adc06d4b1a8bb2eb80d60a3d2fc4ac0a6a3
Andrei Floricel <andrei.floricel@gmail.com>
2024-11-12 12:27:26 +0200

    Add SplitVendorChunk build strategy

--- Frisbee-Cash/vite.config.ts
+++ Frisbee-Cash/vite.config.ts
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig,splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),splitVendorChunkPlugin()],
   base: '/',
   build: {
     sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
-      },
-    },
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks(id) {
+    //       if (id.includes('node_modules')) {
+    //         return id.toString().split('node_modules/')[1].split('/')[0].toString();
+    //       }
+    //     },
+    //     chunkFileNames: 'assets/js/[name]-[hash].js',
+    //     entryFileNames: 'assets/js/[name]-[hash].js',
+    //     assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+    //   },
+    // },
   },
 })
